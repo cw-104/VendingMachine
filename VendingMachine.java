e3ef96b316f0cfeee98e119980f14950f3d9ad2c
Jason Jackson <j.a.jackson1029@gmail.com>
2025-02-12 19:16:19 -0500

    Initial creation of repo and file, comments made listing who does what tasks.

---
+++ VendingMachine.java
@@ -0,0 +1,28 @@
+// Jason Jackson
+// Vending Machine Project
+// Int Version Ctrl
+// 2/12/2025
+
+//Features Set 1:Ryan 
+/*
+ * Large Variety of Snacks
+ * Price associated with each snack
+ * User given welcome msg, and given usage instructions
+ * User can select which snack to buy
+ */
+
+//Features Set 2: Jason 
+/*
+ * User can insert as much money as they want, can only be $1 bills
+ Exact change is needed if they insert too much, give after purchase is made
+ Ask for transaction confirmation, if canceled give full refund
+ Give user departure method after transaction is either completed or canceled and refund is given.
+ */
+
+public class VendingMachine {
+
+    public static void main(String[] args) {
+
+    }
+
+}
